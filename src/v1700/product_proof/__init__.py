5ea93239dfb77d13352101f6cd28c11752709631
limsanghyuk <125784463+limsanghyuk@users.noreply.github.com>
2026-05-20 23:27:09 +0900

    feat(stage140): export product proof validators

---
+++ src/v1700/product_proof/__init__.py
@@ -0,0 +1,7 @@
+from .benchmark_contract import validate_longform_benchmark_contract
+from .sample_project_contract import validate_sample_project_contract
+
+__all__ = [
+    "validate_longform_benchmark_contract",
+    "validate_sample_project_contract",
+]
